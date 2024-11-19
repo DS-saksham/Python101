6404a4ffcee3bd9683f41f30e941fb6f2e47c907
Saksham <saksham44humagain@gmail.com>
2024-11-19 21:29:18 +0545

    sets

--- sets.py
+++ sets.py
@@ -2,5 +2,7 @@
 # and mutable but we can not have the duplicates here
 myset = {2,4,5,67,8,2,8,7}
 print(myset)
-myset_2 = set("Hello World!")
+myset_2 = set("Hello World!") # Here the data are not in the right order
 print(myset_2)
+myset.add(69)
+print(myset)
